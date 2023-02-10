0287510bd95a909c23b752390f6f30fd906ddc09
HARSHIT MISHRA <20bec017@iiitdwd.ac.in>
2023-02-10 23:51:09 +0530

    Commit

---
+++ src/components/feedCard/feedCard.jsx
@@ -0,0 +1,27 @@
+// import { display } from '@mui/system';
+// import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
+
+// function feedCard() {
+//   return (
+//     <div>
+//         <h1>dfag</h1>
+//     </div>
+//     // <Card style={{ width: '200px'}}>
+//     //     <div className="content">
+//     //       <iframe width="200px" height="140px" src="https://www.youtube.com/embed/lUkxSnJ7aDw" title="Create Responsive Navigation Bar With MaterialUI 5 and ReactJS | MaterialUI 5 Navbar | React Navbar" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+//     //     </div>
+//     // </Card>
+//   );
+// }
+// export default feedCard;
+
+function feedCard() {
+    return (
+      <div>
+        <h1>sdagsdg</h1>
+      </div>
+    );
+  }
+  
+  export default feedCard;
